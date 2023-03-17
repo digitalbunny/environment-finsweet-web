a0ab628555add60c15be48beadd61be04c57202f
gordian <gordianbasili@gmail.com>
2023-03-17 22:05:48 +0300

    implement news section

---
+++ components/cards/News.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+    const props = defineProps<{
+        image: string,
+        title: string,
+        description: string
+    }>();
+</script>
+
+<template>
+    <div>
+        <img 
+            :src="props.image" 
+            alt="img"
+            class="object-cover w-full rounded-md mb-4">
+
+        <h2 class="card-title mb-2">{{ props.title }}</h2>
+
+        <p class="card-text line-clamp-3">{{ props.description }}</p>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -263,10 +263,40 @@
         </section>
     </div>
 
-    <div class="container py-12 md:py-32">
+    <div class="container py-12 md:py-32 space-y-10">
         <!-- News -->
         <section>
             <WidgetLinedHeading title="Read our news" class="font-semibold capitalize mb-10" />
+
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-10">
+                <CardsNews 
+                    image="https://images.pexels.com/photos/5118335/pexels-photo-5118335.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1" 
+                    title="Don't destroy greeny and don't spoil scenery" 
+                    description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut facere in 
+                    modi hic quis odio quidem voluptate itaque. Dolore officiis beatae at sequi ex illum 
+                    ducimus quidem excepturi iure ipsa." />
+
+                <CardsNews 
+                    image="https://images.pexels.com/photos/4827/nature-forest-trees-fog.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1" 
+                    title="Is climate change happening faster than expected?" 
+                    description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut facere in 
+                    modi hic quis odio quidem voluptate itaque. Dolore officiis beatae at sequi ex illum 
+                    ducimus quidem excepturi iure ipsa." />
+
+                <CardsNews 
+                    image="https://images.pexels.com/photos/3619870/pexels-photo-3619870.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1" 
+                    title="Top 10 facts about wind farms you didn't know" 
+                    description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut facere in 
+                    modi hic quis odio quidem voluptate itaque. Dolore officiis beatae at sequi ex illum 
+                    ducimus quidem excepturi iure ipsa." />
+
+                <CardsNews 
+                    image="https://images.pexels.com/photos/949194/pexels-photo-949194.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1" 
+                    title="Our goal is to make water available for everyone" 
+                    description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut facere in 
+                    modi hic quis odio quidem voluptate itaque. Dolore officiis beatae at sequi ex illum 
+                    ducimus quidem excepturi iure ipsa." />
+            </div>
         </section>
 
         <!-- Events -->
